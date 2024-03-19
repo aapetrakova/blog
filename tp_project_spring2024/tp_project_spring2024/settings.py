1898d60e3734fd7b7773e110bede2506a4a80b0b
AnastasiiaPetrakova <nastya.petrakova.05@mail.ru>
2024-03-19 18:12:34 +0300

    Add blog app to project settings

--- tp_project_spring2024/tp_project_spring2024/settings.py
+++ tp_project_spring2024/tp_project_spring2024/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure--2n(s&#mh!mmeuy)2e=r%k19&_a^vq2$=z@mi40h(b*0#1z(k4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
