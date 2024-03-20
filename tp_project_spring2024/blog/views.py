f2291d9d488f5734128feb0be297b86b2af22381
AnastasiiaPetrakova <nastya.petrakova.05@mail.ru>
2024-03-20 11:54:59 +0300

    Add view for post's details page

--- tp_project_spring2024/blog/views.py
+++ tp_project_spring2024/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.core.paginator import Paginator
 
 from .models import Post
+
 
 class MainPageView(View):
     def get(self, request, *args, **kwargs):
@@ -12,4 +13,15 @@ class MainPageView(View):
         page_obj = paginator.get_page(page_number)
         return render(request, 'blog/main_page.html', context={
             'page_obj': page_obj,
+        })
+
+
+class PostDetailView(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        last_posts = Post.objects.all().order_by('-id')[:5]
+        return render(request, 'blog/post_details.html', context={
+            'post': post,
+            'last_posts': last_posts,
         })
