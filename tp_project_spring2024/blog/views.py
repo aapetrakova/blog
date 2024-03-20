f9994b250d6d563b6c27078d720c46fa9df24a1e
AnastasiiaPetrakova <nastya.petrakova.05@mail.ru>
2024-03-20 11:16:23 +0300

    Add post's view to the main page

--- tp_project_spring2024/blog/views.py
+++ tp_project_spring2024/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
+
+from .models import Post
 
 class MainPageView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'blog/main_page.html')
+        posts = Post.objects.all()
+        paginator = Paginator(posts, 6)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'blog/main_page.html', context={
+            'page_obj': page_obj,
+        })
