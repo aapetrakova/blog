6327d8e93200d0a4c8f185c2d2e3e8f4b62e04d5
AnastasiiaPetrakova <nastya.petrakova.05@mail.ru>
2024-03-20 11:57:52 +0300

    Add post details url

--- tp_project_spring2024/blog/urls.py
+++ tp_project_spring2024/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MainPageView
+from .views import MainPageView, PostDetailView
 
 urlpatterns = [
     path('', MainPageView.as_view(), name='main_page'),
+    path('blog/<slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
