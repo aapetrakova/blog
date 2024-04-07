8a44207d947254e8e2ec50a24076e2cbd8a77f25
AnastasiiaPetrakova <nastya.petrakova.05@mail.ru>
2024-04-07 18:47:44 +0300

    Add comments for posts

--- tp_project_spring2024/blog/views.py
+++ tp_project_spring2024/blog/views.py
@@ -4,6 +4,10 @@ from django.core.paginator import Paginator
 
 from user.models import UserPost
 
+from message.models import Comment
+from message.forms import CommentForm
+from django.http import HttpResponseRedirect
+
 class MainPageView(View):
     def get(self, request, *args, **kwargs):
         posts = UserPost.objects.all()
@@ -20,7 +24,21 @@ class PostDetailView(View):
     def get(self, request, slug, *args, **kwargs):
         post = get_object_or_404(UserPost, url=slug)
         last_posts = UserPost.objects.all().order_by('-id')[:5]
+        comment_form = CommentForm()
         return render(request, 'blog/post_details.html', context={
             'post': post,
             'last_posts': last_posts,
+            'comment_form': comment_form,
+        })
+
+    def post(self, request, slug, *args, **kwargs):
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            text = request.POST['text']
+            username = self.request.user
+            post = get_object_or_404(UserPost, url=slug)
+            comment = Comment.objects.create(post=post, username=username, text=text)
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+        return render(request, 'myblog/post_details.html', context={
+            'comment_form': comment_form
         })
